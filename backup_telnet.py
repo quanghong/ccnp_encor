6a357e9ee36ecd275d74845c582b472515468262
Quang Hong <quang.hongthien@outlook.com>
2024-10-05 21:48:08 -0400

    done backup init

--- backup_telnet.py
+++ backup_telnet.py
@@ -1,7 +1,7 @@
 import json, os, time, re
 from pprint import pprint, pformat
 from lib.telnet import TELNET
-
+from threading import Thread
 
 def backup_json(data, file_path):                                                  
     with open(file_path, "w") as ObjFile:
@@ -24,6 +24,36 @@ def create_file(nameFile, pathFile, config):
     fl.write(config)
     fl.close()
 
+def backup(dev):
+    session = TELNET(host=dev['host'], username=dev['username'], password=dev['password'], port=dev['port'])
+    connection = session.connect()
+    print(connection, pformat(dev))
+
+    config = ""
+    
+    connection.write(b"\n")
+    time.sleep(2)
+    connection.write(b"en\n")
+    connection.write(b"show run\n")
+    while True:
+        n, match, previous_text = connection.expect([b"\r\n\s--More--\s", b"\r\nend", b"end\r\n\r\n"], 10)
+        config += previous_text.decode()
+        # print(repr(config))
+        if n == 0:
+            connection.write(b" ")
+            # print(repr(config))
+        else:
+            # elif n in range(1, 3):
+            # print(match)
+            config = re.sub(r"\x1b]0;\S+\x07", '', config)
+            # config = re.sub(r"\s--More--\s\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08", '', config)
+            config = re.sub(r"\s--More--\s\x08\x08\x08\x08\x08\x08\x08\x08\x08        \x08\x08\x08\x08\x08\x08\x08\x08\x08", '', config)
+            break
+    # print(config)
+    session.disconnect()
+
+    create_file(dev['name'] + '_init', './BCMSN/backup/', config)
+
 
 def main():
     # Load data from inventory
@@ -31,33 +61,14 @@ def main():
     data = restore_json(file_path)
     # pprint(data)
 
+    threads = []
     for dev in data:
-        session = TELNET(host=dev['host'], username=dev['username'], password=dev['password'], port=dev['port'])
-        connection = session.connect()
-        print(connection, pformat(dev))
+        t = Thread(target=backup, args= (dev,))
+        t.start()
+        threads.append(t)
 
-        config = ""
-        
-        connection.write(b"\n")
-        time.sleep(1)
-        connection.write(b"show run\n")
-        while True:
-            n, match, previous_text = connection.expect([b"\r\n\s--More--\s", b"\r\nend", b"end\r\n\r\n"], 10)
-            config += previous_text.decode()
-            # print(repr(config))
-            if n == 0:
-                connection.write(b" ")
-                # print(repr(config))
-            else:
-                # elif n in range(1, 3):
-                # print(match)
-                config = re.sub(r"\s--More--\s\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08", '', config)
-                config = re.sub(r"\s--More--\s\x08\x08\x08\x08\x08\x08\x08\x08\x08        \x08\x08\x08\x08\x08\x08\x08\x08\x08", '', config)
-                break
-        print(config)
-
-        create_file(dev['name'] + '_init', './BCMSN/backup/', config)
-        
+    for t in threads:
+        t.join()
 
 
 if __name__ == '__main__':
