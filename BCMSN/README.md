e187d7c1662790f61077e1ed05feb2b0ce66e9f8
Quang Hong <quang.hongthien@outlook.com>
2024-10-09 22:14:09 -0400

    binding socket with local interface, ip

---
+++ BCMSN/README.md
@@ -0,0 +1,19 @@
+# Building Campus Multilayer Switch Network
+
+![Topology](/BCMSN/DBM_Inc_Campus_Diagram_lab.JPG)
+
+**Init config**:
+```
+backup_telnet.py
+```
+
+**Verify vlan and trunk**:
+```
+verify.py
+```
+
+**Adding route for local interface**:
+```
+ping -I ens37 10.10.13.1
+sudo route add -net 10.0.0.0/8 gw 192.168.20.137
+```
--- lib/ssh.py
+++ lib/ssh.py
@@ -1,8 +1,13 @@
+import os, socket
 from netmiko import ConnectHandler
 from jinja2 import FileSystemLoader, Environment
+from dotenv import load_dotenv
+
+load_dotenv()
+EVE_NG_IP_HOST_ONLY = os.getenv('EVE_NG_IP_HOST_ONLY')
 
 class SSH:
-    def __init__(self, device_type=None, ip=None, username=None, password=None, port=None, timeout=None):
+    def __init__(self, device_type=None, ip=None, username=None, password=None, port=None, timeout=None, source_address=None):
         self.device_type     = device_type
         self.ip = ip
         self.username = username
@@ -10,9 +15,22 @@ class SSH:
         self.connection = None
         self.port = port if port else 22
         self.timeout = timeout if timeout else 20
+        self.source_address = source_address
 
     def connect(self):
-        self.connection = ConnectHandler(device_type=self.device_type, ip=self.ip, username=self.username, password=self.password)
+        if self.source_address:
+            # Create a socket object
+            source_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            # Bind the socket to the source IP and any available port (port 0 lets the OS pick the port)
+            source_socket.bind((EVE_NG_IP_HOST_ONLY, 0))
+            source_socket.connect((self.ip, self.port))
+            self.connection = ConnectHandler(device_type=self.device_type, ip=self.ip, username=self.username, password=self.password,
+                                            sock=source_socket,
+                                            port=self.port,
+                                            verbose=True)
+        else:
+            self.connection = ConnectHandler(device_type=self.device_type, ip=self.ip, username=self.username, password=self.password)
+        
         return self.connection
 
     def disconnect(self):
