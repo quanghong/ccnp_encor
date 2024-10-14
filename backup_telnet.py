d6c1df23fb940af1c553b14b4dd6c56877585ac5
Quang Hong <quang.hongthien@outlook.com>
2024-10-14 13:05:20 -0400

    enable ssh on devices

--- backup_telnet.py
+++ backup_telnet.py
@@ -4,8 +4,12 @@ from threading import Thread
 
 from lib.telnet import TELNET
 from feature import *
+from jinja2 import FileSystemLoader, Environment
 
-path_code = input('path_code: ')
+PATH_CODE = input('PATH_CODE: ')
+TEMPLATE_DIR = '{}/{}/{}'.format(os.path.curdir, PATH_CODE, input('TEMPLATE_DIR: '))
+USERNAME = input('username: ')
+PASSWORD = input('password: ')
 
 def backup(dev):
     session = TELNET(host=dev['host'], username=dev['username'], password=dev['password'], port=dev['port'])
@@ -35,25 +39,57 @@ def backup(dev):
     # print(config)
     session.disconnect()
 
-    create_file(dev['name'] + '_init', './{}/backup/'.format(path_code), config)
-    # create_file(dev['name'] + '_final', './{}/backup/'.format(path_code), config)
+    create_file(dev['name'] + '_init', './{}/backup/'.format(PATH_CODE), config)
+    # create_file(dev['name'] + '_final', './{}/backup/'.format(PATH_CODE), config)
+
+def enable_ssh(dev):
+    data = {
+        'username': USERNAME,
+        'password': PASSWORD
+    }
+    jj_env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
+    jj_template = jj_env.get_template('enable_ssh.j2')
+    commands = jj_template.render(data)
+    # pprint(commands.splitlines())
+
+    session = TELNET(host=dev['host'], username=dev['username'], password=dev['password'], port=dev['port'])
+    connection = session.connect()
+    print(connection, pformat(dev))
+
+    for cmd in commands.splitlines():
+        cmd += "\n"
+        connection.write(cmd.encode('ascii'))
+        time.sleep(0.5)
+
+    session.disconnect()
 
 
 def main():
     # Load data from inventory
-    file_path = './{}/inventory.json'.format(path_code)
+    file_path = './{}/inventory.json'.format(PATH_CODE)
     data = restore_json(file_path)
-    # pprint(data)
+    pprint(data)
 
+    '''Enable SSH'''
+    threads = []
+    for dev in data:
+        t = Thread(target=enable_ssh, args= (dev,))
+        t.start()
+        threads.append(t)
+    for t in threads:
+        t.join()
+
+    '''Backup configure'''
     threads = []
     for dev in data:
         t = Thread(target=backup, args= (dev,))
         t.start()
         threads.append(t)
-
     for t in threads:
         t.join()
 
+    # We can enable and backup config in one time connection.
+    # OR backup config with ssh by ansible library.
 
 if __name__ == '__main__':
     main()
