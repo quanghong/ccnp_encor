041f2dd502e09e4de0eea476094adcc1d3a0a5e2
Quang Hong <quang.hongthien@outlook.com>
2024-10-14 21:26:58 -0400

    verify eigrp routes

--- BSCI_EIGRP/configure.py
+++ BSCI_EIGRP/configure.py
@@ -40,10 +40,13 @@ def configure_basic_eigrp(dev):
     # Connect
     connection = ConnectHandler(**dev)
     connection.enable()
+    output = connection.send_config_set(commands)
+    connection.save_config()
+    # Verify
     routes = connection.send_command('show ip route', use_textfsm=False)
     connection.disconnect()
 
-    print('ip={}, routes={}'.format(dev['ip'], pformat(routes)))
+    print('ip={}, output={}'.format(dev['ip'], pformat(output)))
 
 def configure_eigrp_security(dev):
     dev['username'] = USERNAME
@@ -77,7 +80,36 @@ def configure_eigrp_security(dev):
 
     print('ip={}, output={}'.format(dev['ip'], pformat(output)))
 
+def verify_eigrp_routes(dev):
+    dev['username'] = USERNAME
+    dev['password'] = PASSWORD
+    dev['device_type'] = 'cisco_ios'
+
+    # Create a socket object
+    source_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Bind the socket to the source IP and any available port (port 0 lets the OS pick the port)
+    source_socket.bind((EVE_NG_IP_HOST_ONLY, 0))
+    source_socket.connect((dev['ip'], 22))
+    dev['sock'] = source_socket
+    dev['verbose'] = True
+    del dev['name']
+    del dev['type']
+    dev['session_log'] = './solution/{}.log'.format(dev['ip'])
+    dev['session_log_file_mode'] = 'append'
+    pprint(dev)
+
+    # Connect
+    connection = ConnectHandler(**dev)
+    # Verify
+    routes = connection.send_command('show ip route eigrp', use_textfsm=True)
+    connection.disconnect()
+
+    print('ip={}, routes={}'.format(dev['ip'], pformat(routes)))
+    backup_json(routes, './solution/{}.json'.format(dev['ip']))
+
+
 def main():
+    '''Basic EIGRP'''
     # configure_basic_eigrp(eigrp_devices[1])
 
     # threads = []
@@ -88,13 +120,18 @@ def main():
     # for t in threads:
     #     t.join()
 
+    '''EIGRP Security'''
     for dev in eigrp_devices:
         try:
             configure_eigrp_security(dev)
         except Exception as exc:
             print(traceback.format_exc())
             pass
-        
+    
+    for dev in eigrp_devices:
+        verify_eigrp_routes(dev)
+
+
 
 if __name__ == '__main__':
     main()
