f589735c31a8de26e676278377fa54ad90c15cc4
Quang Hong <quang.hongthien@outlook.com>
2024-10-17 11:35:01 -0400

    done optimization

--- BSCI_OSPF/README.md
+++ BSCI_OSPF/README.md
@@ -26,7 +26,61 @@ R5#show ip ospf neighbor
 Neighbor ID     Pri   State           Dead Time   Address         Interface
 10.255.255.4      0   FULL/  -        00:01:59    172.16.245.4    Tunnel1
 10.255.255.1      0   FULL/  -        00:01:59    172.16.123.1    Tunnel0
+
+R1#show ip ospf int tun 0 
+Tunnel0 is up, line protocol is up 
+  Internet Address 172.16.123.1/24, Area 1245, Attached via Network Statement
+  Process ID 100, Router ID 10.255.255.1, Network Type POINT_TO_MULTIPOINT, Cost: 1000
 ```
 
 ## OSPF multi-area
-Confiugre and check log files to see Inter-Area (IA) routes (LSA Type 3)
+Confiugre and check log files to see Inter-Area (IA) routes (LSA Type 3) from area 789, 7810, 1245
+```bash
+                Net Link States (Area 0)
+
+Link ID         ADV Router      Age         Seq#       Checksum
+10.100.48.4     10.255.255.4    1511        0x80000005 0x000164
+10.100.78.7     10.255.255.7    1509        0x80000006 0x00A19B
+```
+
+## OSPF Optimization
+The least influent on interrupt connection should be prioritized to configure. So I have change order of list devices by
+> list_configure_order = ['R2', 'R5', 'R1', 'R4', 'SW1', 'SW4', 'SW2', 'SW3']
+
+As we can see, when we change network type to <b>point-to-point</b> in internal network. There will eliminate LSA Type 2 and so neighbor relationship state is <b>FULL/  -</b>. Since this is point-to-point is also <b>non-broadcast</b>, so we don't see <b>FULL/DROTHER</b>
+```bash
+R1#show ip ospf neighbor
+
+Neighbor ID     Pri   State           Dead Time   Address         Interface
+10.255.255.7      0   FULL/  -        00:00:39    10.100.17.7     GigabitEthernet0/1
+10.255.255.5      0   FULL/  -        700 msec    172.16.123.5    Tunnel0
+10.255.255.2      0   FULL/  -        778 msec    172.16.123.2    Tunnel0
+
+R1#show ip ospf database
+
+            OSPF Router with ID (10.255.255.1) (Process ID 100)
+
+# There is no LSA Type 2 (Net Link States) in Area 0 
+		Router Link States (Area 0)
+...
+		Summary Net Link States (Area 0)
+
+# There is no LSA Type 2 (Net Link States) in Area 1245 
+		Router Link States (Area 1245)
+...
+		Summary Net Link States (Area 1245)
+```
+
+If we configure mismatch OSPF network type, there will put routers in 2WAY state.
+```bash
+SW1#show ip ospf neighbor
+
+Neighbor ID     Pri   State           Dead Time   Address         Interface
+10.255.255.8      0   FULL/  -        00:00:30    10.100.78.8     Port-channel12
+10.255.255.1      1   2WAY/DROTHER    00:00:39    10.100.17.1     GigabitEtherne                        t1/1
+10.255.255.9      0   FULL/  -        00:00:32    10.100.79.9     Port-channel13
+10.255.255.10     0   FULL/  -        00:00:35    10.100.107.10   GigabitEtherne                        t1/0
+
+*Oct 16 19:47:33.731: %OSPF-4-NET_TYPE_MISMATCH: Received Hello from 10.255.255.7 on GigabitEthernet0/1 indicating a  potential network type mismatch
+*Oct 16 19:48:40.655: %OSPF-5-ADJCHG: Process 100, Nbr 10.255.255.7 on GigabitEthernet0/1 from FULL to DOWN, Neighbor Down: Dead timer expired   
+```
--- BSCI_OSPF/configure.py
+++ BSCI_OSPF/configure.py
@@ -16,7 +16,7 @@ PASSWORD = os.getenv('PASSWORD')
 LOG_DIR = os.getenv('LOG_DIR')
 
 devices_inv = restore_json(os.getenv('INVENTORY_DIR') + 'devices.json')
-pprint(devices_inv)
+# pprint(devices_inv)
 
 
 def configure_ospf_multi_area(dev):
@@ -58,21 +58,75 @@ def configure_ospf_multi_area(dev):
     print('ip={}, neighbors={}'.format(dev['ip'], pformat(neighbors)))
     print('ip={}, routes={}'.format(dev['ip'], pformat(routes)))
 
+def configure_ospf_optimization(dev):
+    dev['username'] = USERNAME
+    dev['password'] = PASSWORD
+
+    jj_env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
+    jj_template = jj_env.get_template('ospf_optimization.j2')
+    commands = jj_template.render(dev).splitlines()
+    print('name={}, commands={}'.format(dev['name'], pformat(commands)))
+
+    # Create a socket object
+    source_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Bind the socket to the source IP and any available port (port 0 lets the OS pick the port)
+    source_socket.bind((EVE_NG_IP_HOST_ONLY, 0))
+    source_socket.connect((dev['ip'], 22))
+    dev['sock'] = source_socket
+    dev['verbose'] = True
+    del dev['name']
+    del dev['type']
+    dev['session_log'] = '{}/{}.log'.format(LOG_DIR, dev['ip'])
+    dev['session_log_file_mode'] = 'append'
+    pprint(dev)
+
+    # Connect
+    connection = ConnectHandler(**dev)
+    connection.enable()
+    # # AFTER OSPF CONVERGENCE COMMEND SEND_CONFIG_SET
+    # print('ip={}, change network type, lost connection!!!'.format(dev['ip']))
+    # connection.send_config_set(commands, read_timeout=10)
+
+    # LOST CONNECTION AFTER SENDING COMMANDS, NEED SAVE_CONFIG AFTER OSPF CONVERGENCE AGAIN!
+    connection.save_config()
+    # Verify
+    neighbors = connection.send_command('show ip ospf neighbor', use_textfsm=False)
+    routes = connection.send_command('show ip route ospf', use_textfsm=False)
+    routes = connection.send_command('show ip ospf database', use_textfsm=False)
+    connection.disconnect()
+
+    print('ip={}, neighbors={}'.format(dev['ip'], pformat(neighbors)))
+    print('ip={}, routes={}'.format(dev['ip'], pformat(routes)))
+
+
 def main():
-    '''Basic OSPF'''
-    threads = []
-    for dev in devices_inv:
+    '''OSPF Multi area'''
+    # threads = []
+    # for dev in devices_inv:
+    #     try:
+    #         configure_ospf_multi_area(dev)
+    #     except Exception as exc:
+    #         print(traceback.format_exc())
+    # #     t = Thread(target=configure_ospf_multi_area, args= (dev,))
+    # #     t.start()
+    # #     threads.append(t)
+    # # for t in threads:
+    # #     t.join()
+
+    '''OSPF Optimization'''
+    list_configure = []
+    list_configure_order = ['R2', 'R5', 'R1', 'R4', 'SW1', 'SW4', 'SW2', 'SW3']
+    for name in list_configure_order:
+        for dev in devices_inv:
+            if dev['name'] == name:
+                list_configure.append(dev)
+    pprint(list_configure)
+
+    for dev in list_configure:
         try:
-            configure_ospf_multi_area(dev)
+            configure_ospf_optimization(dev)
         except Exception as exc:
             print(traceback.format_exc())
-
-    #     t = Thread(target=configure_ospf_multi_area, args= (dev,))
-    #     t.start()
-    #     threads.append(t)
-    # for t in threads:
-    #     t.join()
-
 
 if __name__ == '__main__':
     main()
